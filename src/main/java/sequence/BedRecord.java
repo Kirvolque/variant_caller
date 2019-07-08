1bacb6f8a9c5964d9a71a0bcc73cdb8e8c946483
Stakhiev Alexander <stahiev.av@edu.spbstu.ru>
2019-07-08 03:50:23 +0300

    test: Add test case for invalid input

--- src/main/java/sequence/BedRecord.java
+++ src/main/java/sequence/BedRecord.java
@@ -16,7 +16,7 @@ public class BedRecord {
     // replace 2 spaces with 1 to avoid wrong line splitting
     line = line.replace(DOUBLE_SPACE, SINGLE_SPACE);
     String[] subStr = line.split(SINGLE_SPACE);
-    if (subStr.length > 2 || !subStr[0].equals("track")) {
+    if (subStr.length > 2 && !subStr[0].equals("track")) {
       return new BedRecord(subStr[0], new Interval(Integer.valueOf(subStr[1]), Integer.valueOf(subStr[2])));
     } else {
       throw new IllegalArgumentException("The string does not satisfy the BED format");
